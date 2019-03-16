aa80fb7549bfcf8e17e6ee44357210128ebb2901
Coen Valk <valkc@rpi.edu>
2019-03-16 12:31:15 -0400

    Add project files.

---
+++ CondenseMyTalk/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ValkCoen\Documents\HACKRPI\Code\CondenseMyTalk\CondenseMyTalk\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
